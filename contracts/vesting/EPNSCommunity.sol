be65b56e5b4f1e2dcb87943ad7e80eef6ca0e478
Manank Patni <manank321@gmail.com>
2021-02-10 01:53:50 +0530

    Add community contract
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- contracts/vesting/EPNSCommunity.sol
+++ contracts/vesting/EPNSCommunity.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.6.11;
 
 import "../openzeppelin/TokenVesting.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract EPNSCommunity is TokenVesting {
     /**
@@ -13,7 +14,16 @@ contract EPNSCommunity is TokenVesting {
      * @param duration duration in seconds of the period in which the tokens will vest
      * @param revocable whether the vesting is revocable or not
      */
-    constructor(address beneficiary, uint256 start, uint256 cliffDuration, uint256 duration, bool revocable) TokenVesting(beneficiary, start, cliffDuration, duration, revocable) public {}
+    constructor(
+        address beneficiary,
+        uint256 start,
+        uint256 cliffDuration,
+        uint256 duration,
+        bool revocable
+    )
+        public
+        TokenVesting(beneficiary, start, cliffDuration, duration, revocable)
+    {}
 
     /**
      * @notice Change the beneficiary of the contract
@@ -21,8 +31,26 @@ contract EPNSCommunity is TokenVesting {
      * @return Whether or not the transfer succeeded
      */
     function setBeneficiary(address newBeneficiary) external returns (bool) {
-        require(msg.sender == _beneficiary, "Push::setBeneficiary: Not contract beneficiary");
-        require(_beneficiary != newBeneficiary, "Push::setBeneficiary: Same beneficiary address as old");
+        require(
+            msg.sender == _beneficiary,
+            "Push::setBeneficiary: Not contract beneficiary"
+        );
+        require(
+            _beneficiary != newBeneficiary,
+            "Push::setBeneficiary: Same beneficiary address as old"
+        );
         _beneficiary = newBeneficiary;
+    }
+
+    function release(address beneficiary, IERC20 token) public override {
+        uint256 unreleased = _releasableAmount(token);
+
+        require(unreleased > 0, "TokenVesting: no tokens are due");
+
+        _released[address(token)] = _released[address(token)].add(unreleased);
+
+        token.safeTransfer(_beneficiary, unreleased);
+
+        emit TokensReleased(address(token), unreleased);
     }
 }
