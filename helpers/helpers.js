5d151b4757669cd33bb763ff858aba7b45eaa6c1
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-02-16 08:14:11 +0530

    Partial commit of constants

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -15,49 +15,8 @@ function bn(number, defaultValue = null) {
 		}
 		number = defaultValue;
 	}
-  
+
 	return ethers.BigNumber.from(number);
 }
 
 module.exports.bn = bn;
-
-/**
- * Asserts that given promise will throw because of revert().
- * @param {Promise} promise
- */
-async function expectRevert(promise) {
-	await expectError(promise, ['revert'])
-}
-
-module.exports.expectRevert = expectRevert;
-
-/**
- * Asserts that given promise will throw because of revert() or failed assertion.
- * @param {Promise} promise
- */
-async function expectRevertOrFail(promise) {
-	await expectError(promise, ['revert', 'invalid opcode'])
-}
-
-module.exports.expectRevertOrFail = expectRevertOrFail;
-
-/**
- * Asserts that given promise will throw and that thrown message will contain one of the given
- * search strings.
- *
- * @param {Promise} promise The promise expecting to throw.
- * @param {string[]} messages List of expected thrown message search strings.
- */
-async function expectError(promise, messages) {
-	try {
-		await promise
-	} catch (error) {
-		for (let i = 0; i < messages.length; i++) {
-			if (error.message.search(messages[i]) >= 0) {
-				return
-			}
-		}
-		assert.fail("Expected revert, got '" + error + "' instead.")
-	}
-	assert.fail('Expected revert not received.')
-}
