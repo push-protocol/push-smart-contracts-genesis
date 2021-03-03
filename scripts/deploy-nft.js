69dc497b0751d5c2344b914d5923d45d1e07bcf1
Jude Dike <dikejude49@gmail.com>
2021-03-03 16:54:21 +0100

    nft deployment script

---
+++ scripts/deploy-nft.js
@@ -0,0 +1,115 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+const fs = require("fs");
+const chalk = require("chalk");
+const { config, ethers } = require("hardhat");
+
+// const {
+//   VESTING_INFO,
+//   DISTRIBUTION_INFO,
+//   META_INFO
+// } = require("./constants");
+
+// Primary Function
+async function main() {
+  // First deploy all contracts
+  console.log(chalk.bgBlack.bold.green(`\n📡 Deploying NFT \n-----------------------\n`));
+  const deployedContracts = await setupAllContracts();
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n All Contracts Deployed \n\t\t\t\n`));
+
+  // Try to verify
+  console.log(chalk.bgBlack.bold.green(`\n📡 Verifying Contracts \n-----------------------\n`));
+  await verifyAllContracts(deployedContracts);
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n All Contracts Verified \n\t\t\t\n`));
+}
+
+// Secondary Functions
+// Deploy All Contracts
+async function setupAllContracts() {
+  let deployedContracts = [];
+  const signer = await ethers.getSigner(0)
+
+  // Deploy EPNS ($PUSH) Tokens first
+  const NFTToken = await deployContract("Rockstar", [signer.address], "$ROCKs")
+  deployedContracts.push(NFTToken)
+  return deployedContracts;
+}
+
+// Verify All Contracts
+async function verifyAllContracts(deployedContracts) {
+  return new Promise(async function(resolve, reject) {
+
+    for await (contract of deployedContracts) {
+      const arguments = contract.deployargs
+
+      if (hre.network.name != "hardhat") {
+        // Mostly a real network, verify
+        const { spawnSync } = require( 'child_process' )
+        const ls = spawnSync( `npx`, [ 'hardhat', 'verify', '--network', hre.network.name, contract.address ] )
+
+        console.log( `stderr: ${ ls.stderr.toString() }` )
+        console.log( `stdout: ${ ls.stdout.toString() }` )
+      }
+      else {
+        console.log(chalk.bgWhiteBright.black(`${contract.filename}.sol`), chalk.bgRed.white(` is on Hardhat network... skipping`))
+      }
+    }
+
+    resolve();
+  });
+}
+
+// Helper Functions
+// For Deploy
+async function deploy(name, _args, identifier) {
+  const args = _args || [];
+
+  console.log(`📄 ${name}`);
+  const contractArtifacts = await ethers.getContractFactory(name);
+  const contract = await contractArtifacts.deploy();
+  await contract.deployed()
+  console.log(
+    chalk.cyan(name),
+    "deployed to:",
+    chalk.magenta(contract.address)
+  );
+  fs.writeFileSync(`artifacts/${name}_${identifier}.address`, contract.address);
+  return contract;
+}
+
+async function deployContract(contractName, contractArgs, identifier) {
+  let contract = await deploy(contractName, contractArgs, identifier);
+
+  contract.filename = `${contractName} -> ${identifier}`;
+  contract.deployargs = contractArgs;
+
+  return contract;
+}
+
+function readArgumentsFile(contractName) {
+  let args = [];
+  try {
+    const argsFile = `./contracts/${contractName}.args`
+    if (fs.existsSync(argsFile)) {
+      args = JSON.parse(fs.readFileSync(argsFile));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  return args;
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
