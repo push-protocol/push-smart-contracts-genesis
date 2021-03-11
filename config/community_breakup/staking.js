53c81409b75c49aaca6bdcf93c15856c05202ead
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-03-11 17:02:21 +0530

    Community config rebase

--- config/community_breakup/staking.js
+++ config/community_breakup/staking.js
@@ -4,7 +4,7 @@ const { bn, tokens, tokensBN, dateToEpoch, timeInSecs, CONSTANT_100K, CONSTANT_1
 const stakingInfo = {
   staking: {
     epochDuration: timeInSecs(7,24,60,60),
-    communityVaultAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    communityVaultAddress: "0x998abeb3E57409262aE5b751f60747921B33613E",
     pushTokenAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
   },
   pushToken: {
