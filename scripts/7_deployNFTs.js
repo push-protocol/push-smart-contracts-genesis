113aec859dee59927a62f26d6f9f735dee78b207
Manank Patni <manank321@gmail.com>
2021-04-06 03:57:15 +0530

    Add versioning to airdrop, nfts and yield farming scripts
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- scripts/7_deployNFTs.js
+++ scripts/7_deployNFTs.js
@@ -9,26 +9,39 @@ const fs = require("fs");
 const chalk = require("chalk");
 const { config, ethers } = require("hardhat");
 
+const { bn, tokens, bnToInt, timeInDays, timeInDate, deployContract, verifyAllContracts, distributeInitialFunds } = require('../helpers/utils')
+const { versionVerifier, upgradeVersion } = require('../loaders/versionVerifier')
+
 const {
   NFT_INFO
 } = require("./constants")
 
 // Primary Function
 async function main() {
+  // Version Check
+  console.log(chalk.bgBlack.bold.green(`\n✌️  Running Version Checks \n-----------------------\n`))
+  const versionDetails = versionVerifier(["pushTokenAddress"])
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n Version Control Passed \n\t\t\t\n`))
+
   // First deploy all contracts
   console.log(chalk.bgBlack.bold.green(`\n📡 Deploying ROCKSTAR NFTs and Minting \n-----------------------\n`));
-  const deployedContracts = await setupAllContracts();
+  const deployedContracts = await setupAllContracts(versionDetails)
   console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n All Contracts Deployed \n\t\t\t\n`));
 
   // Try to verify
   console.log(chalk.bgBlack.bold.green(`\n📡 Verifying Contracts \n-----------------------\n`));
-  await verifyAllContracts(deployedContracts);
+  await verifyAllContracts(deployedContracts, versionDetails)
   console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n All Contracts Verified \n\t\t\t\n`));
+
+  // Upgrade Version
+  console.log(chalk.bgBlack.bold.green(`\n📟 Upgrading Version   \n-----------------------\n`))
+  upgradeVersion()
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n ✅ Version upgraded    \n\t\t\t\n`))
 }
 
 // Secondary Functions
 // Deploy All Contracts
-async function setupAllContracts() {
+async function setupAllContracts(versionDetails) {
   let deployedContracts = [];
   const signer = await ethers.getSigner(0)
 
@@ -41,7 +54,7 @@ async function setupAllContracts() {
   deployedContracts.push(BatchMintNFT)
 
   // Deploy NFTRewards
-  const NFTRewardsArgs = [bn(NFT_INFO.nfts.tokens).div(bn(NFT_INFO.nfts.users)), NFT_INFO.nfts.pushTokenAddress, Rockstar.address]
+  const NFTRewardsArgs = [bn(NFT_INFO.nfts.tokens).div(bn(NFT_INFO.nfts.users)), versionDetails.deploy.args.pushTokenAddress, Rockstar.address]
   const NFTRewards = await deployContract("NFTRewards", NFTRewardsArgs, "RockstarNFTRewards")
   deployedContracts.push(NFTRewards)
 
@@ -49,7 +62,7 @@ async function setupAllContracts() {
   await batchMintNFTs(Rockstar, BatchMintNFT)
 
   // Push token transfer to NFTRewards
-  await tokensToNFTRewards(NFTRewards, NFT_INFO.nfts.pushTokenAddress)
+  await tokensToNFTRewards(NFTRewards, versionDetails.deploy.args.pushTokenAddress)
 
   // return deployed contracts
   return deployedContracts;
@@ -111,71 +124,6 @@ async function batchMintNFTs(rockstar, batchMintNFT) {
 
   console.log(chalk.bgBlack.white(`Transaction hash:`), chalk.gray(`${tx.hash}`))
   console.log(chalk.bgBlack.white(`Transaction etherscan:`), chalk.gray(`https://${hre.network.name}.etherscan.io/tx/${tx.hash}`))
-}
-
-// Verify All Contracts
-async function verifyAllContracts(deployedContracts) {
-  return new Promise(async function(resolve, reject) {
-
-    for await (contract of deployedContracts) {
-      const arguments = contract.deployargs
-
-      if (hre.network.name != "hardhat") {
-        // Mostly a real network, verify
-        const { spawnSync } = require( 'child_process' )
-        const ls = spawnSync( `npx`, [ 'hardhat', 'verify', '--network', hre.network.name, contract.address ] )
-
-        console.log( `stderr: ${ ls.stderr.toString() }` )
-        console.log( `stdout: ${ ls.stdout.toString() }` )
-      }
-      else {
-        console.log(chalk.bgWhiteBright.black(`${contract.filename}.sol`), chalk.bgRed.white(` is on Hardhat network... skipping`))
-      }
-    }
-
-    resolve();
-  });
-}
-
-// Helper Functions
-// For Deploy
-async function deploy(name, _args, identifier) {
-  const args = _args || [];
-
-  console.log(`📄 ${name}`);
-  const contractArtifacts = await ethers.getContractFactory(name);
-  const contract = await contractArtifacts.deploy(...args);
-  await contract.deployed()
-  console.log(
-    chalk.cyan(name),
-    "deployed to:",
-    chalk.magenta(contract.address)
-  );
-  fs.writeFileSync(`artifacts/${name}_${identifier}.address`, contract.address);
-  return contract;
-}
-
-async function deployContract(contractName, contractArgs, identifier) {
-  let contract = await deploy(contractName, contractArgs, identifier);
-
-  contract.filename = `${contractName} -> ${identifier}`;
-  contract.deployargs = contractArgs;
-
-  return contract;
-}
-
-function readArgumentsFile(contractName) {
-  let args = [];
-  try {
-    const argsFile = `./contracts/${contractName}.args`
-    if (fs.existsSync(argsFile)) {
-      args = JSON.parse(fs.readFileSync(argsFile));
-    }
-  } catch (e) {
-    console.log(e);
-  }
-
-  return args;
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ scripts/8_deployYieldFarm.js
@@ -0,0 +1,155 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const moment = require('moment')
+const hre = require("hardhat");
+
+const fs = require("fs");
+const chalk = require("chalk");
+const { config, ethers } = require("hardhat");
+
+const { bn, tokens, bnToInt, timeInDays, timeInDate, deployContract, verifyAllContracts, distributeInitialFunds } = require('../helpers/utils')
+const { versionVerifier, upgradeVersion } = require('../loaders/versionVerifier')
+
+const {
+  VESTING_INFO,
+  DISTRIBUTION_INFO,
+  META_INFO,
+  STAKING_INFO
+} = require("./constants")
+
+// Primary Function
+async function main() {
+  // Version Check
+  console.log(chalk.bgBlack.bold.green(`\n✌️  Running Version Checks \n-----------------------\n`))
+  const versionDetails = versionVerifier(["pushTokenAddress", "communityVaultAddress"])
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n Version Control Passed \n\t\t\t\n`))
+
+  // First deploy all contracts
+  console.log(chalk.bgBlack.bold.green(`\n📡 Deploying Contracts \n-----------------------\n`));
+  const deployedContracts = await setupAllContracts(versionDetails);
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n All Contracts Deployed \n\t\t\t\n`));
+
+  // Try to verify
+  console.log(chalk.bgBlack.bold.green(`\n📡 Verifying Contracts \n-----------------------\n`));
+  await verifyAllContracts(deployedContracts, versionDetails);
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n All Contracts Verified \n\t\t\t\n`));
+
+  // Upgrade Version
+  console.log(chalk.bgBlack.bold.green(`\n📟 Upgrading Version   \n-----------------------\n`))
+  //upgradeVersion()
+  console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n ✅ Version upgraded    \n\t\t\t\n`))
+}
+
+// Secondary Functions
+// Deploy All Contracts
+async function setupAllContracts(versionDetails) {
+  let deployedContracts = [];
+
+  // Get EPNS ($PUSH) instance first
+  const contractArtifactsEPNS = await ethers.getContractFactory("EPNS")
+  const PushToken = await contractArtifactsEPNS.attach(versionDetails.deploy.args.pushTokenAddress)
+
+  // Get Community Vault instance first
+  const contractArtifactsCommunityVault = await ethers.getContractFactory("CommunityVault")
+  const CommunityVault = await contractArtifactsCommunityVault.attach(versionDetails.deploy.args.communityVaultAddress)
+
+  // Deploy Staking Contract
+  StakingInstance = await setupStakingContract(PushToken, CommunityVault, deployedContracts, versionDetails)
+
+  // Deploy and Setup LP Rewards Contracts
+  deployedContracts = await setupLPRewards(PushToken, StakingInstance, CommunityVault, deployedContracts, versionDetails)
+
+  // Deploy and Setup EPNS Staking Contracts
+  deployedContracts = await setupEPNSStaking(PushToken, StakingInstance, CommunityVault, deployedContracts, versionDetails)
+
+  // Lastly transfer ownership of community reservoir contract
+  console.log(chalk.bgBlue.white(`Changing CommunityVault ownership to eventual owner`))
+
+  const txCommunityVault = await CommunityVault.transferOwnership(META_INFO.multisigOwnerEventual)
+
+  console.log(chalk.bgBlack.white(`Transaction hash:`), chalk.gray(`${txCommunityVault.hash}`))
+  console.log(chalk.bgBlack.white(`Transaction etherscan:`), chalk.gray(`https://${hre.network.name}.etherscan.io/tx/${txCommunityVault.hash}`))
+
+  // return deployed contracts
+  return deployedContracts;
+}
+
+// Module Deploy - Staking
+async function setupStakingContract(PushToken, CommunityVault, deployedContracts, versionDetails) {
+  const signer = await ethers.getSigner(0)
+  console.log(chalk.bgBlue.white(`Deploying Staking Contract`));
+
+  // Deploying Staking Contract
+  const stakingInitialArgs = STAKING_INFO.stakingInfo.staking
+  const stakingArgs = [STAKING_INFO.stakingInfo.pushToken.epoch1Start, stakingInitialArgs.epochDuration]
+  const StakingInstance = await deployContract("Staking", stakingArgs, "Staking")
+
+  return StakingInstance
+}
+
+// Module Deploy - Staking
+async function setupEPNSStaking(PushToken, StakingInstance, CommunityVault, deployedContracts, versionDetails) {
+  const yieldFarmPUSHInitialArgs = STAKING_INFO.stakingInfo.pushToken
+
+  console.log(chalk.bgBlue.white(`Deploying PUSH Yield Farming Contract`));
+  // Deploying PUSH token Yield Farming Contract
+  const yieldFarmPUSHArgs = [
+    PushToken.address,
+    PushToken.address,
+    StakingInstance.address,
+    CommunityVault.address,
+    yieldFarmPUSHInitialArgs.startAmount.mul(ethers.BigNumber.from(10).pow(18)).toString(),
+    yieldFarmPUSHInitialArgs.deprecation.mul(ethers.BigNumber.from(10).pow(18)).toString(),
+    yieldFarmPUSHInitialArgs.nrOfEpochs.toString()
+  ]
+  
+  const yieldFarmPUSHInstance = await deployContract("YieldFarm", yieldFarmPUSHArgs, "YieldFarm")
+  deployedContracts.push(yieldFarmPUSHInstance)
+
+  console.log(chalk.bgBlue.white(`Setting allowance for Staking contracts to spend tokens from CommunityVault`))
+  const txCommunityVault = await CommunityVault.setAllowance(yieldFarmPUSHInstance.address, STAKING_INFO.stakingInfo.helpers.getPushDistributionAmount())
+
+  console.log(chalk.bgBlack.white(`Transaction hash:`), chalk.gray(`${txCommunityVault.hash}`))
+  console.log(chalk.bgBlack.white(`Transaction etherscan:`), chalk.gray(`https://${hre.network.name}.etherscan.io/tx/${txCommunityVault.hash}`))
+
+  return deployedContracts
+}
+
+// Module Deploy - LP Rewards
+async function setupLPRewards(PushToken, StakingInstance, CommunityVault, deployedContracts, versionDetails) {
+  const yieldFarmLPInitialArgs = STAKING_INFO.stakingInfo.liquidityPoolTokens
+
+  console.log(chalk.bgBlue.white(`Deploying LP Yield Farming Contract`));
+  // Deploying LP token Yield Farming Contract
+  const yieldFarmLPArgs = [
+    PushToken.address,
+    versionDetails.deploy.args.lpPoolTokenAddress,
+    StakingInstance.address,
+    CommunityVault.address,
+    yieldFarmLPInitialArgs.startAmount.mul(ethers.BigNumber.from(10).pow(18)).toString(),
+    yieldFarmLPInitialArgs.deprecation.mul(ethers.BigNumber.from(10).pow(18)).toString(),
+    yieldFarmLPInitialArgs.nrOfEpochs.toString()
+  ]
+  const yieldFarmLPInstance = await deployContract("YieldFarm", yieldFarmLPArgs, "YieldFarm")
+  deployedContracts.push(yieldFarmLPInstance)
+
+  console.log(chalk.bgBlue.white(`Setting allowance for Staking contracts to spend tokens from CommunityVault`))
+  const txCommunityVault = await CommunityVault.setAllowance(yieldFarmLPInstance.address, STAKING_INFO.stakingInfo.helpers.getLiquidityDistributionAmount())
+
+  console.log(chalk.bgBlack.white(`Transaction hash:`), chalk.gray(`${txCommunityVault.hash}`))
+  console.log(chalk.bgBlack.white(`Transaction etherscan:`), chalk.gray(`https://${hre.network.name}.etherscan.io/tx/${txCommunityVault.hash}`))
+
+  return deployedContracts
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
