2e10d05d470e50fb9e2fff8b8bee6396f088292a
aiswaryawalter <aiswaryawalter@gmail.com>
2022-06-17 19:28:18 +0530

    Rockstar v2 files

---
+++ scripts/versioncontrol/4_2_deployNFTsV2.config.js
@@ -0,0 +1,30 @@
+const deploy = {
+  network: {
+    mainnet: {
+      version: 1
+    },
+    goerli: {
+      version: 1
+    },
+    kovan: {
+      version: 3
+    },
+    ropsten: {
+      version: 1
+    },
+    rinkeby: {
+      version: 13
+    },
+    hardhat: {
+      version: 1
+    },
+    localhost: {
+      version: 1
+    }
+  },
+  args: {
+    dummyBlock: null
+  }
+}
+
+exports.deploy = deploy
