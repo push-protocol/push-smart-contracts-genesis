28fc4e845881f3f1be2c67fec58cca402f0f7504
aiswaryawalter <aiswaryawalter@gmail.com>
2022-06-19 00:28:19 +0530

    add rockstarV2 configs

---
+++ config/community_breakup/gratitude/nftsV2.js
@@ -0,0 +1,28 @@
+const { tokens, CONSTANT_1K, CONSTANT_10K, CONSTANT_100K, CONSTANT_1M } = require('../../../helpers/utils')
+const { nftsMappingV2 } = require('./nftsMappingInfoV2.enc')
+
+const nftsV2 = {
+  tokens: tokens(9 * CONSTANT_10K), // 90k tokens for 100 Users
+  users: 100,
+  nftsMappingV2: nftsMappingV2,
+  pushTokenAddress: "0xc3792a32a83C0a9531F31f5bfc91114728Dee0E9", //ropsten address
+  helpers: {
+    convertNFTObjectToIndividualArrays: function(nftObject) {
+      let nftsObject = {
+        recipients: [],
+        metadatas: []
+      }
+
+      for (const [key, value] of Object.entries(nftObject)) {
+        nftsObject.recipients.push(value[0])
+        nftsObject.metadatas.push(value[1])
+      }
+
+      return nftsObject
+    }
+  }
+}
+
+module.exports = {
+  nftsV2: nftsV2
+}
