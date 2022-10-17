7532c8892b54365d64b8f38f9cbbc7f3c04c5de1
Zaryab <zaryabafser2000@gmail.com>
2022-10-17 10:54:42 +0400

    added new sample structure

--- config/investors.sample.js
+++ config/investors.sample.js
@@ -9,7 +9,7 @@ const investors = {
   },
   factory: {
     inv1: {
-      address: '0xf90B86fDbb194a703eA6709B1cB108079A5F9396',
+      address: '0xa89523351BE1e2De64937AA9AF61Ae06eAd199C7',
       tokens: tokens(14389), // 6k Tokenss
       vested: {
         start: dateToEpoch('08/11/2021 00:00'), // Mon, 8 Nov 2021 12 AM GMT
@@ -19,7 +19,7 @@ const investors = {
       revocable: false
     },
     inv2: {
-      address: '0x4F20Cb7a1D567A54350a18DAcB0cc803aEBB4483',
+      address: '0xa89523351BE1e2De64937AA9AF61Ae06eAd199C7',
       tokens: tokens(1200000), // 1,200,000 Tokens
       vested: {
         start: dateToEpoch('20/12/2021 00:00'), // Mon, 20 Dec 2021 12 AM GMT
